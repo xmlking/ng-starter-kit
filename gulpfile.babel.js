4ec17cc30bf46e8c3ba177e33695bb89e8aa018b
Sumanth <xmlking@gmail.com>
2015-08-10 04:31:33 -0700

    eslint

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,19 +1,8 @@
 import 'gulp-ng-recipes/dist/globals.js';
 import 'gulp-ng-recipes/dist/server.js';
-//import 'gulp-ng-recipes/dist/typescript.js';
+import 'gulp-ng-recipes/dist/scripts.js';
 import 'gulp-ng-recipes/dist/sass.js';
 import 'gulp-ng-recipes/dist/images.js';
 import 'gulp-ng-recipes/dist/offline.js';
 import 'gulp-ng-recipes/dist/build.js';
 import 'gulp-ng-recipes/dist/gpdeploy.js';
-
-import gulp  from 'gulp';
-
-gulp.task('scripts', (cb) => {
-  cb
-});
-
-gulp.task('tslint', (cb) => {
-  cb
-});
-
--- jspm.config.js
+++ jspm.config.js
@@ -21,6 +21,7 @@ System.config({
     "angular-animate": "github:angular/bower-angular-animate@1.4.3",
     "angular-aria": "github:angular/bower-angular-aria@1.4.3",
     "angular-material": "github:angular/bower-material@0.10.0",
+    "angular-messages": "github:angular/bower-angular-messages@1.4.3",
     "angular-mocks": "github:angular/bower-angular-mocks@1.4.3",
     "angular-ui-router": "github:angular-ui/ui-router@0.2.15",
     "babel": "npm:babel-core@5.8.21",
@@ -28,8 +29,8 @@ System.config({
     "core-js": "npm:core-js@0.9.18",
     "css": "github:systemjs/plugin-css@0.1.13",
     "json": "github:systemjs/plugin-json@0.1.0",
-    "oclazyload-systemjs-router": "github:lookfirst/oclazyload-systemjs-router@1.2.2",
     "ocLazyLoad": "github:ocombe/ocLazyLoad@1.0.4",
+    "oclazyload-systemjs-router": "github:lookfirst/oclazyload-systemjs-router@1.2.2",
     "text": "github:systemjs/plugin-text@0.0.2",
     "github:angular-ui/ui-router@0.2.15": {
       "angular": "github:angular/bower-angular@1.4.3"
